009c68abbb29f41aac498c1c4c4141e7126d5b96
ahnahn <80297360+ahnahn@users.noreply.github.com>
2021-05-29 17:55:32 +0900

    Add files via upload
    새로 작성한 파일들입니다

--- app.py
+++ app.py
@@ -2,29 +2,22 @@
 
 from flask import Flask
 from flask import render_template
+from flask import request
 app = Flask(__name__)
-
 @app.route('/')
 def index():
 	return render_template('home.html')
-
-@app.route('/북문')
-def index2():
-
-	return render_template('북문.html')
-
-@app.route('/쪽문')
-def index3():
-	return render_template('쪽문.html')
-
-@app.route('/정문')
-def index4():
-	return render_template('정문.html')
-
-@app.route('/테크노문')
-def index5():
-	return render_template('테크노문.html')
+@app.route('/info', methods=['GET', 'POST'])
+def info():
+	error = None
+	if request.method == 'POST':
+		myname = request.form['rating']
+		return render_template('info.html', name=myname)
+	else:
+		myname = request.args.get('rating')
+		return render_template('info.html', name=myname)
 
 if __name__=="__main__":
   app.run(debug=True)
   app.run(host="127.0.0.1", port="5000", debug=True)
+
